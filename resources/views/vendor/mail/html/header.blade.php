caa265eb6e0c5344487baf6636041687afbba076
dietchse <wsuhandiet@gmail.com>
2026-06-06 02:48:28 +0700

    style: revert to clean light email theme, remove panel border, fix button text

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
-<span style="color: #ffffff; font-weight: 900; text-transform: uppercase; font-size: 24px; letter-spacing: -0.5px;">Cek</span><span style="color: #eab308; font-weight: 900; text-transform: uppercase; font-size: 24px; letter-spacing: -0.5px;">bayar</span>
+<span style="color: #000000; font-weight: 900; text-transform: uppercase; font-size: 24px; letter-spacing: -0.5px;">Cek</span><span style="color: #eab308; font-weight: 900; text-transform: uppercase; font-size: 24px; letter-spacing: -0.5px;">bayar</span>
 </a>
 </td>
 </tr>
